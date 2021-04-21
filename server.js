acccdc2b7e6bc27f459b7655fbab7b54e9981937
Clare Henderson <henderson2350@gmail.com>
2021-04-21 17:58:33 -0400

    created the models

--- server.js
+++ server.js
@@ -1,6 +1,13 @@
 const express = require('express');
+const sequelize = require('./config/connection');
 const routes = require('./routes');
 // import sequelize connection
+
+const Category = require('./models/Category')
+const Product = require('./models/Product')
+const Tag = require('./models/Tag')
+const ProductTag = require('./models/ProductTag')
+
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -11,6 +18,9 @@ app.use(express.urlencoded({ extended: true }));
 app.use(routes);
 
 // sync sequelize models to the database, then turn on the server
-app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}!`);
-});
+sequelize.sync({force:false}).then(() => {
+  app.listen(PORT, () => {
+    console.log(`App listening on port ${PORT}!`);
+  });
+})
+
