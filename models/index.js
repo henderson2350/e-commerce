adf5c9b17fe60088dfc51aa7c4bfffa6ed56f9f4
Clare Henderson <henderson2350@gmail.com>
2021-04-21 20:39:33 -0400

    added one route for git, not functioning

--- models/index.js
+++ models/index.js
@@ -14,8 +14,9 @@ Category.hasMany(Product, {
   onDelete: 'CASCADE'
 })
 // Products belongToMany Tags (through ProductTag)
-
+Product.belongsToMany(Tag, { through: ProductTag})
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, { through: ProductTag})
 
 module.exports = {
   Product,
