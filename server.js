1677235bb586e46be95eaa9050028f8537e75951
Clare Henderson <henderson2350@gmail.com>
2021-04-22 16:27:32 -0400

    fixed database error (syntax

--- server.js
+++ server.js
@@ -3,10 +3,10 @@ const sequelize = require('./config/connection');
 const routes = require('./routes');
 // import sequelize connection
 
-const Category = require('./models/Category')
-const Product = require('./models/Product')
-const Tag = require('./models/Tag')
-const ProductTag = require('./models/ProductTag')
+// const Category = require('./models/Category')
+// const Product = require('./models/Product')
+// const Tag = require('./models/Tag')
+// const ProductTag = require('./models/ProductTag')
 
 
 const app = express();
@@ -15,10 +15,11 @@ const PORT = process.env.PORT || 3001;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// turning on our routes
 app.use(routes);
 
 // sync sequelize models to the database, then turn on the server
-sequelize.sync({force:false}).then(() => {
+sequelize.sync({force : true}).then(() => {
   app.listen(PORT, () => {
     console.log(`App listening on port ${PORT}!`);
   });
